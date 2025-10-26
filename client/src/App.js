4b9ad849f6e2b3f6b4f7df4c69a38a805433b2cf
Alan Ponte <alanjponte@gmail.com>
2025-10-25 20:53:45 -0700

    update connection

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,23 @@ const App = () => {
     setInput('');
 
     try {
-      const response = await axios.post('/chat', { user_input: input });
+      const response = await axios.post('http://localhost:5001/chat', { user_input: input });
       setMessages([...newMessages, { text: response.data.response, sender: 'bot' }]);
     } catch (error) {
       console.error('Error sending message:', error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error('Response data:', error.response.data);
+        console.error('Response status:', error.response.status);
+        console.error('Response headers:', error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error('Request:', error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error('Error message:', error.message);
+      }
     }
   };
 
